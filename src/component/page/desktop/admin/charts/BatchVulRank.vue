a6ea03b7216e06647b77055d6089d76d9026fd8d
LiuQ-Q <545784977@qq.com>
2019-07-08 16:59:50 +0800

    [FIXED] improve BatchVulRank of charts

--- src/component/page/desktop/admin/charts/BatchVulRank.vue
+++ src/component/page/desktop/admin/charts/BatchVulRank.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <b-breadcrumb
+	<div>
+		<b-breadcrumb
 			:items="[
 				{ text: '管理', active: true },
 				{ text: '数据概览', href: '#/desktop/admin/overview' },
@@ -8,59 +8,139 @@
 			]"
 		/>
 
-    <chart-canvas
-      style="height:800px"
-      :options="{
-        chart: {
-          type: 'bar'
-        },
-        title: {
-          text: null
-        },
-        xAxis: {
-          title: {
-            text: '批次'
-          },
-          categories: ['34', '32', '33', '31', '30', '21', '22', '25', '23', '24']
-        },
-        yAxis: {
-          min: 0,
-          title: {
-            text: '记录数'
-          }
-        },
-        legend: {
-          reversed: true 
-        },
-        plotOptions: {
-          series: {
-            stacking: 'normal'
-          }
-        },
-        series: [{
-          name: '总行内网',
-          data: [5, 6, 4, 5, 2, 0, 3, 0, 0, 1]
-        }, {
-          name: '信用卡中心',
-          data: [5, 5, 4, 7, 2, 5, 3, 4, 4, 1]
-        }, {
-          name: '天津分行',
-          data: [7, 4, 3, 2, 3, 3, 2, 2, 2, 1]
-        }, {
-          name: '上海分行',
-          data: [4, 3, 3, 2, 3, 4, 3, 2, 1, 1]
-        }, {
-          name: '南宁分行',
-          data: [6, 4, 4, 2, 5, 2, 3, 4, 2, 1]
-        }]
-      }"
-    />
-  </div>
+		<chart-canvas
+			style="height:800px"
+			:options="{
+				chart: {
+					type: 'bar'
+				},
+				title: {
+					text: null
+				},
+				xAxis: {
+					title: {
+						text: '批次'
+					},
+					categories: batchList
+				},
+				yAxis: {
+					min: 0,
+					title: {
+						text: '记录数'
+					}
+				},
+				legend: {
+					reversed: true 
+				},
+				plotOptions: {
+					series: {
+						stacking: 'normal'
+					}
+				},
+				series: series
+			}"
+		/>
+	</div>
 </template>
 
 <script>
-export default {
+import bocData from '../../../../../plugin/backend/boc.json';
 
+export default {
+	data() {
+		return {
+			bocData,
+			batchInfo: [],
+			batchList: [],
+			series: []
+		}
+	},
+	created() {
+		this.getBatchInfo();
+		this.getBatchList();
+		this.getSeries();
+	},
+	methods: {
+		getBatchInfo() {
+			this.bocData.forEach(bocEle => {
+				const batchEven = this.batchInfo.some(batchEle => {
+					return batchEle.batch === bocEle.batch;
+				});
+
+				if (!batchEven) {
+					this.batchInfo.push({
+						batch: bocEle.batch,
+						vul: 1,
+						branch: [{
+							name: bocEle.branch,
+							vul: 1
+						}]
+					});
+				} else {
+					this.batchInfo.forEach(batchEle => {
+						if (batchEle.batch === bocEle.batch) {
+							batchEle.vul++;
+
+							const branchEven = batchEle.branch.some(branchEle => {
+								return branchEle.name === bocEle.branch;
+							})
+
+							if (!branchEven) {
+								batchEle.branch.push({
+									name: bocEle.branch,
+									vul: 1
+								});
+							} else {
+								batchEle.branch.forEach(branchEle => {
+									if (branchEle.name === bocEle.branch) {
+										branchEle.vul++;
+									}
+								});
+							}
+						}
+					});
+				}
+			});
+
+			this.batchInfo.sort((a, b) => {
+				return b.vul - a.vul;
+			});
+
+			this.batchInfo.splice(10);
+			// console.log(this.batchInfo);
+		},
+		getBatchList() {
+			this.batchList = this.batchInfo.map(batchEle => {
+				return batchEle.batch;
+			});
+		},
+		getSeries() {
+			this.batchInfo.forEach((batchEle, batchIndex) => {
+				batchEle.branch.forEach((branchEle) => {
+					const even = this.series.some(serie => {
+						return serie.name === branchEle.name;
+					});
+
+					if (!even) {
+						this.series.push({
+							name: branchEle.name,
+							data:[0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+						});
+
+						this.series[this.series.length - 1].data[batchIndex] += branchEle.vul;
+					} else {
+						this.series.forEach(serie => {
+							if (serie.name === branchEle.name) {
+								serie.data[batchIndex] += branchEle.vul;
+							}
+						});
+					}
+				});
+			});
+
+			// console.log(this.series);
+		}
+	}
 }
 </script>
 
--- src/plugin/chart/Chart.vue
+++ src/plugin/chart/Chart.vue
@@ -13,26 +13,26 @@ HighchartsMore(Highcharts);
 TreeMap(Highcharts);
 
 export default {
-  name: 'chart-pie',
-  props: ['options'],
-  data() {
-    return {
-      chart: null
-    }
-  },
-  mounted() {
-    this.initChart();
-  },
-  methods: {
-    initChart() {
-      this.chart = new Highcharts.Chart(this.$el, Object.assign({}, this.options, {
-        colors: [
-          '#dc3545', '#ffc107', '#17a2b8', '#DDDF00',
-          '#24CBE5', '#64E572', '#FF9655', '#FFF263',
-          '#6AF9C4'
-        ]
-      }));
-    }
-  }
+	name: 'chart-pie',
+	props: ['options'],
+	data() {
+		return {
+			chart: null
+		}
+	},
+	mounted() {
+		this.initChart();
+	},
+	methods: {
+		initChart() {
+			this.chart = new Highcharts.Chart(this.$el, Object.assign({}, this.options, {
+				colors: [
+					'#dc3545', '#ffc107', '#17a2b8', '#DDDF00',
+					'#24CBE5', '#64E572', '#FF9655', '#FFF263',
+					'#6AF9C4', '#4949C5', '#12FF00'
+				]
+			}));
+		}
+	}
 }
 </script>
