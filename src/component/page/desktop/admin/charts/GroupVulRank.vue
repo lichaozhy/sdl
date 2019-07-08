b6c9108fb90d9c0baca6085389cfea35d88425c6
LiuQ-Q <545784977@qq.com>
2019-07-08 15:35:08 +0800

    [FIXED] improve ProjectVulRank of charts

--- src/component/page/desktop/admin/charts/GroupVulRank.vue
+++ src/component/page/desktop/admin/charts/GroupVulRank.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <b-breadcrumb
+	<div>
+		<b-breadcrumb
 			:items="[
 				{ text: '管理', active: true },
 				{ text: '数据概览', href: '#/desktop/admin/overview' },
@@ -8,53 +8,96 @@
 			]"
 		/>
 
-    <chart-canvas
-      style="height:800px"
-      :options="{
-        credits: { enabled: false },
-        chart: { type: 'bar' },
-        title: { text: null },
-        xAxis: {
-          categories: ['开发6部', '开发3部', '开发8部', '开发4部', '开发7部', '开发2部', '开发1部', '开发5部'],
-          title: {
-            text: '项目组'
-          }
-        },
-        yAxis: {
-          min: 0,
-          title: {
-            text: '记录数',
-          }
-        },
-        plotOptions: {
-          bar: {
-            dataLabels: {
-              enabled: true,
-            }
-          }
-        },
-        legend: false,
-        series: [{
-          colorByPoint: true,
-          data: [
-            { y: 16 },
-            { y: 15 },
-            { y: 14 },
-            { y: 13 },
-            { y: 11 },
-            { y: 10 },
-            { y: 10 },
-            { y: 10 }
-          ]
-        }]
-      }"
-    />
-  </div>
+		<chart-canvas
+			style="height:800px"
+			:options="{
+				credits: { enabled: false },
+				chart: { type: 'bar' },
+				title: { text: null },
+				xAxis: {
+					categories: groupList,
+					title: {
+						text: '项目组'
+					}
+				},
+				yAxis: {
+					min: 0,
+					title: {
+						text: '记录数',
+					}
+				},
+				plotOptions: {
+					bar: {
+						dataLabels: {
+							enabled: true,
+						}
+					}
+				},
+				legend: false,
+				series: [{
+					colorByPoint: true,
+					data: [
+						{ y: groupInfo[0].vul },
+						{ y: groupInfo[1].vul },
+						{ y: groupInfo[2].vul },
+						{ y: groupInfo[3].vul },
+						{ y: groupInfo[4].vul },
+						{ y: groupInfo[5].vul },
+						{ y: groupInfo[6].vul },
+						{ y: groupInfo[7].vul }
+					]
+				}]
+			}"
+		/>
+	</div>
 </template>
 
 <script>
-export default {
+import bocData from '../../../../../plugin/backend/boc.json';
 
+export default {
+	data() {
+		return {
+			bocData,
+			groupInfo: [],
+			groupList: []
+		}
+	},
+	created() {
+		this.getgroupInfo();
+		this.getGroupList();
+	},
+	methods: {
+		getgroupInfo() {
+			this.bocData.forEach(bocEle => {
+				const even = this.groupInfo.some(group => {
+					return group.name === bocEle.projectGroup;
+				});
+
+				if (!even) {
+					this.groupInfo.push({
+						name: bocEle.projectGroup,
+						vul: 1
+					});
+				} else {
+					this.groupInfo.forEach(group => {
+						if (group.name === bocEle.projectGroup) {
+							group.vul++;
+						}
+					});
+				}
+			});
+
+			this.groupInfo.sort((a, b) => {
+				return b.vul - a.vul;
+			});
+		},
+		getGroupList() {
+			this.groupList = this.groupInfo.map(group => {
+				return group.name;
+			});
+		}
+	}
 }
 </script>
 
