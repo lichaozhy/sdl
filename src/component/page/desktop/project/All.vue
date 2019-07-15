4c8d8b445ea443b753803899483e34107656c0fc
LiuQ-Q <545784977@qq.com>
2019-07-15 18:06:42 +0800

    [FIXED] improve stage-track

--- src/component/page/desktop/project/All.vue
+++ src/component/page/desktop/project/All.vue
@@ -18,6 +18,9 @@
 			{ key: 'createdAt', label: '创建时间', class: 'project-created-at' },
 		]"
 		:items="project.owned.list"
+		id="project-all-mine"
+		:per-page="perPage"
+		:current-page="currentPageMine"
 		class="text-center project-all-mine"
 	>
 		<template
@@ -27,6 +30,18 @@
 			<b-link :href='`#/desktop/project/${data.item.id}`'>{{ data.item.name }}</b-link>
 		</template>
 	</b-table>
+
+	<div class="mt-3">
+		<b-pagination
+			size="sm" 
+			align="center"
+			aria-controls="project-all-mine"
+			v-model="currentPageMine" 
+			:total-rows="rowsMine" 
+			:per-page="perPage"
+		></b-pagination>
+	</div>
+
 	<h3>我参与的</h3>
 	<b-table
 		small
@@ -40,6 +55,9 @@
 			{ key: 'joinedAt', label: '加入时间', class: 'project-created-at' },
 		]"
 		:items="project.joined.list"
+		id="project-all-joined"
+		:per-page="perPage"
+		:current-page="currentPageJoined"
 		class="text-center"
 	>
 		<template
@@ -49,6 +67,17 @@
 			<b-link>{{ data.item.name }}</b-link>
 		</template>
 	</b-table>
+
+	<div class="mt-3">
+		<b-pagination
+			size="sm" 
+			align="center"
+			aria-controls="admin-overview-tableproject-all-joined"
+			v-model="currentPageJoined" 
+			:total-rows="rowsJoined" 
+			:per-page="perPage"
+		></b-pagination>
+	</div>
 </div>
 
 </template>
@@ -64,6 +93,9 @@ export default {
 	name: 'page-project-joined',
 	data() {
 		return {
+			perPage: 8,
+			currentPageMine: 1,
+			currentPageJoined: 1,
 			project: {
 				owned: {
 					list: [
@@ -75,6 +107,32 @@ export default {
 						{ id: 3, name: '大连分行微信银行', version: 'v2.2.1', general: '分行', branch: '大连分行', createdAt: '2017-6-20' },
 						{ id: 1, name: '合肥分行微信系统', version: 'v2.0.0', general: '分行', branch: '合肥分行', createdAt: '2017-3-27' },
 						{ id: 2, name: '南昌分行微信公众号', version: 'v2.2.0', general: '分行', branch: '南昌分行', createdAt: '2016-9-5' },
+						{ id: 2, name: '信用卡中心-人才招聘', version: 'v1.1.0', general: '事业部', branch: '信用卡中心', createdAt: '2017-9-22' },
+						{ id: 1, name: '合肥分行微信系统', version: 'v2.0.0', general: '分行', branch: '合肥分行', createdAt: '2017-3-27' },
+						{ id: 2, name: '移动催收系统', version: 'v1.3.0', general: '事业部', branch: '信用卡中心', createdAt: '2018-7-6' },
+						{ id: 1, name: '手机银行', version: 'v1.1.0', general: '事业部', branch: '信用卡中心', createdAt: '2018-10-20' },
+						{ id: 3, name: '大连分行微信银行', version: 'v2.2.1', general: '分行', branch: '大连分行', createdAt: '2017-6-20' },
+						{ id: 3, name: '信用卡中心', version: 'v1.2.0', general: '事业部', branch: '信用卡中心', createdAt: '2018-3-15' },
+						{ id: 5, name: '信用卡中心-支付网关', version: 'v1.0.1', general: '事业部', branch: '信用卡中心', createdAt: '2017-11-4' },
+						{ id: 2, name: '南昌分行微信公众号', version: 'v2.2.0', general: '分行', branch: '南昌分行', createdAt: '2016-9-5' },
+						{ id: 1, name: '手机银行', version: 'v1.1.0', general: '事业部', branch: '信用卡中心', createdAt: '2018-10-20' },
+						{ id: 2, name: '移动催收系统', version: 'v1.3.0', general: '事业部', branch: '信用卡中心', createdAt: '2018-7-6' },
+						{ id: 3, name: '信用卡中心', version: 'v1.2.0', general: '事业部', branch: '信用卡中心', createdAt: '2018-3-15' },
+						{ id: 5, name: '信用卡中心-支付网关', version: 'v1.0.1', general: '事业部', branch: '信用卡中心', createdAt: '2017-11-4' },
+						{ id: 2, name: '信用卡中心-人才招聘', version: 'v1.1.0', general: '事业部', branch: '信用卡中心', createdAt: '2017-9-22' },
+						{ id: 3, name: '大连分行微信银行', version: 'v2.2.1', general: '分行', branch: '大连分行', createdAt: '2017-6-20' },
+						{ id: 1, name: '合肥分行微信系统', version: 'v2.0.0', general: '分行', branch: '合肥分行', createdAt: '2017-3-27' },
+						{ id: 2, name: '南昌分行微信公众号', version: 'v2.2.0', general: '分行', branch: '南昌分行', createdAt: '2016-9-5' },
+						{ id: 2, name: '信用卡中心-人才招聘', version: 'v1.1.0', general: '事业部', branch: '信用卡中心', createdAt: '2017-9-22' },
+						{ id: 1, name: '合肥分行微信系统', version: 'v2.0.0', general: '分行', branch: '合肥分行', createdAt: '2017-3-27' },
+						{ id: 2, name: '移动催收系统', version: 'v1.3.0', general: '事业部', branch: '信用卡中心', createdAt: '2018-7-6' },
+						{ id: 1, name: '手机银行', version: 'v1.1.0', general: '事业部', branch: '信用卡中心', createdAt: '2018-10-20' },
+						{ id: 3, name: '大连分行微信银行', version: 'v2.2.1', general: '分行', branch: '大连分行', createdAt: '2017-6-20' },
+						{ id: 3, name: '信用卡中心', version: 'v1.2.0', general: '事业部', branch: '信用卡中心', createdAt: '2018-3-15' },
+						{ id: 5, name: '信用卡中心-支付网关', version: 'v1.0.1', general: '事业部', branch: '信用卡中心', createdAt: '2017-11-4' },
+						{ id: 2, name: '南昌分行微信公众号', version: 'v2.2.0', general: '分行', branch: '南昌分行', createdAt: '2016-9-5' },
+						{ id: 2, name: '南昌分行微信公众号', version: 'v2.2.0', general: '分行', branch: '南昌分行', createdAt: '2016-9-5' },
+						{ id: 5, name: '信用卡中心-支付网关', version: 'v1.0.1', general: '事业部', branch: '信用卡中心', createdAt: '2017-11-4' },
 					]
 				},
 				joined: {
@@ -84,13 +142,31 @@ export default {
 						{ id: 7, name: '人事服务平台', version: 'v3.0.0', general: '分行', branch: '总行内网', joinedAt: '2017-5-22', owner: '陈六' },
 						{ id: 2, name: '金融租赁手机OA系统', version: 'v1.2.0', general: '事业部', branch: '金融市场部', joinedAt: '2018-5-10', owner: '王五' },
 						{ id: 3, name: '资金交易管理系统', version: 'v2.3.0', general: '事业部', branch: '金融市场部', joinedAt: '2017-11-5', owner: '张三' },
+						{ id: 1, name: '金融租赁综合信息管理', version: 'v2.1.0', general: '事业部', branch: '金融租赁', joinedAt: '2017-9-6', owner: '李四' },
 						{ id: 1, name: 'IT运维管理系统', version: 'v2.0.0', general: '总行', branch: '总行内网', joinedAt: '2017-8-6', owner: '李四' },
+						{ id: 1, name: '支付机构监管上报终端', version: 'v2.0.0', general: '分行', branch: '上海分行', joinedAt: '2017-7-5', owner: '王五' },
+						{ id: 3, name: '资金交易管理系统', version: 'v2.3.0', general: '事业部', branch: '金融市场部', joinedAt: '2017-11-5', owner: '张三' },
+						{ id: 7, name: '人事服务平台', version: 'v3.0.0', general: '分行', branch: '总行内网', joinedAt: '2017-5-22', owner: '陈六' },
+						{ id: 2, name: '金融租赁手机OA系统', version: 'v1.2.0', general: '事业部', branch: '金融市场部', joinedAt: '2018-5-10', owner: '王五' },
+						{ id: 4, name: '金融租赁', version: 'v1.1.0', general: '事业部', branch: '金融租赁', joinedAt: '2018-9-6', owner: '张三' },
+						{ id: 1, name: 'IT运维管理系统', version: 'v2.0.0', general: '总行', branch: '总行内网', joinedAt: '2017-8-6', owner: '李四' },
+						{ id: 6, name: '金融租赁邮箱系统', version: 'v1.3.0', general: '事业部', branch: '金融租赁', joinedAt: '2018-7-10', owner: '李四' },
+						{ id: 1, name: '金融租赁综合信息管理', version: 'v2.1.0', general: '事业部', branch: '金融租赁', joinedAt: '2017-9-6', owner: '李四' },
+						{ id: 1, name: '支付机构监管上报终端', version: 'v2.0.0', general: '分行', branch: '上海分行', joinedAt: '2017-7-5', owner: '王五' },
 					]
 				}
 			}
+		};
+	},
+	computed: {
+		rowsMine() {
+			return this.project.owned.list.length;
+		},
+		rowsJoined() {
+			return this.project.joined.list.length;
 		}
 	}
-}
+};
 </script>
 <style lang="less">
 
--- src/component/page/desktop/project/Detail.vue
+++ src/component/page/desktop/project/Detail.vue
@@ -100,13 +100,20 @@
 				>
 					<template slot="bottom-row">
 						<td v-for="(stage, index) in stageList" :key="index">
-							<div>
+							<div
+								v-for="version in stage.versionList"
+								:key="version.value"
+							>
 								<b-card
-									v-for="version in stage.versionList"
-									:key="version.value"
-									class="mb-2"
+									v-if="version.value===undefined"
+									style="min-height:400px;"
+									no-body
+								></b-card>
+								<b-card
+									v-if="version.value!==undefined"
 									:border-variant="version.active?'primary':'default'"
 									no-body
+									style="min-height:400px;"
 								>
 									<b-card-header
 										class="py-1 px-2 rounded-0"
@@ -146,6 +153,7 @@
 										:footer-bg-variant="'light'"
 										:footer-text-variant="version.active?'light':'default'"
 										class="p-0"
+										style="position:absolute;bottom:0;width:100%;"
 									>
 										<b-button size="sm" variant="link" v-if="version.active">回滚</b-button>
 										<b-button size="sm" variant="link" v-if="version.active">演进</b-button>
@@ -184,63 +192,93 @@ import attributeData from './attribute.json';
 
 const stageList = [
 	{
-		name: "requirement",
+		name: 'requirement',
 		versionList: [
 			{
 				active: true,
-				value: "1.1.0",
-				message: "增加信用卡快捷还款功能，推进第二次公测",
+				value: '1.1.0',
+				message: '增加信用卡快捷还款功能，推进第二次公测',
 			},
-			{ value: "1.0.0", message: "正式版本规划，增强安全需求" },
-			{ value: "0.3.0", message: "增加功能与第三方数据接口的规划功能" },
-			{ value: "0.2.0", message: "重新定位产品和进一步确认用户画像" },
-			{ value: "0.1.0", message: "第一次设计研讨" }
+			{},
+			{},
+			{ value: '1.0.0', message: '正式版本规划，增强安全需求' },
+			{},
+			{},
+			{},
+			{},
+			{ value: '0.3.0', message: '增加功能与第三方数据接口的规划功能' },
+			{ value: '0.2.0', message: '重新定位产品和进一步确认用户画像' },
+			{ value: '0.1.0', message: '第一次设计研讨' }
 		]
 	},
 	{
-		name: "design",
+		name: 'design',
 		versionList: [
+			{},
+			{},
+			{},
+			{ value: '1.0.0', message: '考虑安全切面，加入安全设计流程。设计功能迁移方案'},
+			{},
+			{},
+			{},
+			{},
+			{ value: '0.3.0', message: '继承第二版设计，引入数据共享设计' },
+			{ value: '0.2.0', message: '第二版需求的设计' },
+			{}
+		]
+	},
+	{
+		name: 'development',
+		versionList: [
+			{},
+			{ value: '1.0.2', message: '修正 兼容iPhone X的下方安全区域' },
+			{ value: '1.0.1', message: '修正 一些错误；修正 补充CSS常量' },
 			{
-				value: "1.0.0",
-				message: "考虑安全切面，加入安全设计流程。设计功能迁移方案"
-			},
-			{ value: "0.3.0", message: "继承第二版设计，引入数据共享设计" },
-			{ value: "0.2.0", message: "第二版需求的设计" }
-		]
-	},
-	{
-		name: "development",
-		versionList: [
-			{ value: "1.0.2", message: "修正 兼容iPhone X的下方安全区域" },
-			{ value: "1.0.1", message: "修正 一些错误；修正 补充CSS常量" },
-			{
-				value: "1.0.0",
-				message: "使用OnsenUI重构，迁移功能，增加对刘海屏的兼容，引入安全开发框架"
+				value: '1.0.0',
+				message: '使用OnsenUI重构，迁移功能，增加对刘海屏的兼容，引入安全开发框架'
 			},
 			{
-				value: "0.3.4",
-				message: "全面检查功能，确保基本功能正常可用。锁定UI控件版本"
+				value: '0.3.4',
+				message: '全面检查功能，确保基本功能正常可用。锁定UI控件版本'
 			},
-			{ value: "0.3.3", message: "引入二维码扫描插件，升级依赖" },
-			{ value: "0.3.2", message: "更新UI框架" },
-			{ value: "0.3.1", message: "更新前端依赖，升级Cordova" },
-			{ value: "0.3.0", message: "从0.2.x,开始迁移。更换前端架构" }
+			{ value: '0.3.3', message: '引入二维码扫描插件，升级依赖' },
+			{ value: '0.3.2', message: '更新UI框架' },
+			{ value: '0.3.1', message: '更新前端依赖，升级Cordova' },
+			{ value: '0.3.0', message: '从0.2.x,开始迁移。更换前端架构' },
+			{},
+			{},
 		]
 	},
 	{
-		name: "testing",
+		name: 'testing',
 		versionList: [
-			{ value: "1.0.2", message: "1.x交付前测试" },
-			{ value: "0.3.4", message: "0.3.x的最终上线前测试" },
-			{ value: "0.3.1", message: "新依赖测试" },
-			{ value: "0.3.0", message: "迁移后，接入持续集成，尝试第一次测试" }
+			{},
+			{ value: '1.0.2', message: '1.x交付前测试' },
+			{},
+			{},
+			{ value: '0.3.4', message: '0.3.x的最终上线前测试' },
+			{},
+			{},
+			{ value: '0.3.1', message: '新依赖测试' },
+			{ value: '0.3.0', message: '迁移后，接入持续集成，尝试第一次测试' },
+			{},
+			{},
 		]
 	},
 	{
-		name: "acceptance",
+		name: 'acceptance',
 		versionList: [
-			{ value: "1.0.2", message: "完整功能的公测运行" },
-			{ value: "0.3.4", message: "内测" }
+			{},
+			{ value: '1.0.2', message: '完整功能的公测运行' },
+			{},
+			{},
+			{ value: '0.3.4', message: '内测' },
+			{},
+			{},
+			{},
+			{},
+			{},
+			{},
 		]
 	}
 ];
@@ -250,14 +288,14 @@ stageList[0].versionList.forEach(version => {
 		feature: 1,
 		security: 1,
 		docs: randomInt(0, 4),
-	}
+	};
 });
 
 stageList[1].versionList.forEach(version => {
 	version.resource = {
 		docs: randomInt(1, 5),
 		securityDesign: randomInt(0, 1),
-	}
+	};
 });
 
 stageList[2].versionList.forEach(version => {
@@ -265,7 +303,7 @@ stageList[2].versionList.forEach(version => {
 		commits: randomInt(6, 20),
 		lint: randomInt(0, 15),
 		source: randomInt(0, 35)
-	}
+	};
 });
 
 stageList[3].versionList.forEach(version => {
@@ -274,13 +312,13 @@ stageList[3].versionList.forEach(version => {
 		fail: randomInt(0, 12),
 		vulner: randomInt(0, 12),
 		jira: '已分配',
-	}
+	};
 });
 
 stageList[4].versionList.forEach(version => {
 	version.resource = {
 		warning: randomInt(0, 3)
-	}
+	};
 });
 
 const tagMap = {
@@ -296,7 +334,7 @@ const tagMap = {
 	vulner: { variant: 'danger', text: '漏洞' },
 	warning: { variant: 'warning', text: '运行警告' },
 	jira: { variant: 'success', text: 'jira' },
-}
+};
 
 function randomInt(from, to) {
 	return Math.floor(from + Math.random() * (to- from));
@@ -365,12 +403,29 @@ export default {
 			width: 100%;
 			top: 0;
 			left: 0;
+
+			table tbody {
+				height: 96%;
+			}
 		}
 	}
 
 	table {
 		height: 100%;
 		background: #fff;
+
+		thead {
+			display:table;
+			width:100%;
+			table-layout:fixed;
+			width: calc( 100% - 16px )
+		}
+
+		tbody {
+			display: block;
+			height: 540px;
+			overflow-y: scroll;
+		}
 
 		th {
 			text-align: center;
@@ -381,9 +436,9 @@ export default {
 
 			> div {
 				width: 360px;
-				height: 100%;
+				// height: 100%;
 				padding: 5px;
-				overflow-y: scroll;
+				// overflow-y: scroll;
 			}
 		}
 	}
